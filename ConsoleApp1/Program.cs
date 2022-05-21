3013f6826773aaf12156aa2a4e385ad8d2bacd1b
Esteban Lamanna <esteban.lamanna@uatp.com>
2022-05-20 23:39:11 -0300

    project creation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var valor = ConfigurationManager.AppSettings["VARIABLE_TO_BE_OVERWRITTEN"];
+
+            Console.WriteLine(valor);
+
+            Console.ReadLine();
+        }
+    }
+}
